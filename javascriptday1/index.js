5a66becde7fb482c13e06051d79cf5f51cf8a751
unknown <dipikabhandari66@gmail.com>
2024-05-12 11:35:54 +0545

    javascript day1 and 2

---
+++ javascriptday1/index.js
@@ -0,0 +1,39 @@
+
+
+var age = 40;
+console.log(age);
+
+var numberone = 30;
+var numbertwo = 20;
+
+var sum = numberone+numbertwo;
+var sub = numberone-numbertwo;
+var mul = numberone*numbertwo;
+var div = numberone/numbertwo;
+
+console.log(sum);
+console.log(sub);
+console.log(mul);
+console.log(div);
+
+
+var firstname = "Dipika";
+var secondname = "Bhandari.";
+
+var name = firstname + secondname;
+
+console.log(`hello ${firstname} ${secondname}`)
+// console.log(name);
+
+
+// my name is Dipika Bhandari. I live in gaindakot. I am 23 years old.
+
+var a = "My name is";
+var name = "Dipika Bhandari."
+var b = "i live in";
+var location = "gaindakot.";
+var c = "I am";
+var age = "23";
+var d = "years old.";
+
+console.log(`hello ${a} ${name} ${b} ${location} ${c} ${age} ${d}`);
---
+++ javascriptday2/day2.js
@@ -0,0 +1,79 @@
+
+// const person = {
+//     firstName : "Dipika",
+//     secondName : "Bhandari",
+//     age : 23,
+
+
+//     fullName:function() {
+//         return this.firstName + " " + this.secondName;
+//      },
+    
+
+// };
+
+// console.log(person);
+
+// console.table(person);
+
+// console.log(person.firstName);
+
+// console.log(person.fullName());
+
+
+
+// const patient = {
+//     firstName:"Dipak",
+//     secondName:"Bhnadari",
+//     age:23,
+//     sex:"male",
+//     registrationNum:12345,
+//     description:"this patient is diagonised with brain tumor",
+
+//     fullName:function()
+    
+//     {
+//         return this.firstName + "" + this.secondName
+//     },
+
+//     fullDescription:function()
+//     {
+//         return this.firstName +" "+this.secondName+""+this.sex+""+this.registrationNum+""+this.description
+
+//     },
+// }
+
+// console.table(patient);
+
+// console.log(patient.fullDescription());
+
+
+
+
+// var name = "nscnjdshciewjdiewjdijewidjeihiefhweijdqidjoqiwdjwqjdq"
+
+// console.log( name.length);
+
+// var name = "dipika";
+// var lastname = "bhandari";
+
+// console.log(name == lastname);
+
+// console.log(name == lastname ? "yes": "no");
+
+
+
+var bookprice = 8085;
+var microprocessorbook = "8085";
+
+var fullName = bookprice + microprocessorbook;
+var fullNamet = bookprice - microprocessorbook;
+
+
+console.log(typeof bookprice);
+console.log(typeof microprocessorbook);
+
+console.log(typeof fullName);
+console.log(typeof fullNamet);
+
+console.log(bookprice === microprocessorbook ? "yes":"no");
