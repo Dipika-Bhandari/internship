6a3dc03729a37952ef6cb8065a83da9b93e35c66
unknown <dipikabhandari66@gmail.com>
2024-05-15 11:25:37 +0545

    day 3 and 4 of javascript

---
+++ javascript/jsday3/index.js
@@ -0,0 +1,115 @@
+// const fruits=["Banana", "Apple", "Mango", 3, 4];
+
+// console.log(fruits);
+// console.log(fruits[0]);
+// console.log(fruits[1]);
+// console.log(fruits[2]);
+// console.log(fruits[3]);
+// console.log(fruits[4]);
+
+// console.log(fruits.indexOf("Apple"));
+// console.log(fruits.at(0));
+
+// // console.log(fruits.indexOflast);
+
+// // console.log(fruits.at(indexOflast));
+
+
+// console.log(fruits.length)
+
+// console.log(fruits[fruits.length-1]);
+
+// console.log(fruits[4]);
+
+// console.log(fruits.join("#"));
+
+
+
+// const students=["Dipika", "Mamta", "Aditya", "Anurag", "Pramesh", "Rohika", "Sanjok", "Ashok"];
+
+// console.log(students);
+
+// const newstudents=students.pop();
+
+// console.log(newstudents);
+
+// console.log(students);
+
+// const popSanjok=students.pop();
+// console.log(popSanjok);
+// console.log(students);
+
+// const popRoshika=students.pop();
+// console.log(popRoshika);
+// console.log(students);
+
+// const popPramesh=students.pop();
+// console.log(popPramesh);
+// console.log(students);
+
+// const popAnurag=students.pop();
+// console.log(popAnurag);
+// console.log(students);
+
+// const popAditya=students.pop();
+// console.log(popAditya);
+// console.log(students);
+
+// const popMamta=students.pop();
+// console.log(popMamta);
+// console.log(students);
+
+// const popDipika=students.pop();
+// console.log(popDipika);
+// console.log(students);
+
+// const pushdipika=students.push("Dipika");
+// console.log(students);
+
+// const pushmamta=students.push("Mamta");
+// console.log(students);
+
+// const pushAditya=students.push("Aditya");
+// console.log(students);
+
+// const pushAnurag=students.push("Anurag");
+// console.log(students);
+
+// const pushpramesh=students.push("Pramesh");
+// console.log(students);
+
+// const pushroshika=students.push("Roshika");
+// console.log(students);
+
+// const pushsanjok=students.push("Sanjok");
+// console.log(students);
+
+// const pushAshok=students.push("Ashok");
+// console.log(students);
+
+// const unshiftsagar=students.unshift("sagar");
+// console.log(students);
+
+// const shiftsuraj=students.shift("Suraj");
+// console.log(students);
+
+const fruits=["Apple", "Mago", "Banana", "Grapes", "kiwi", "Pineapple"];
+
+// const replace = fruits.copyWithin(0,0);
+// console.log(replace);
+
+// const replace = fruits.copyWithin(1,0);
+// console.log(replace);
+
+const replace= fruits.copyWithin(4,0);
+console.log(replace);
+
+// const replace = fruits.copyWithin(3,0);
+// console.log(replace);
+
+// const replace = fruits.copyWithin(4,0);
+// console.log(replace);
+
+
+
+
---
+++ javascript/jsday4/index.js
@@ -0,0 +1,166 @@
+
+
+// const name = ["amrit", "anurag", "nepal"];
+
+// name.forEach((value) => {
+
+//     console.log(value);
+    
+// });
+
+// const newname = name.map((newname) => {
+//     return newname + "mamta";
+
+
+// });
+
+// console.log(newname);
+
+
+const fruits = [
+    "Apple",
+    "Banana",
+    "Orange",
+    "Grape",
+    "Strawberry",
+    "Blueberry",
+    "Watermelon",
+    "Pineapple",
+    "Mango",
+    "Kiwi",
+    "Peach",
+    "Pear",
+    "Cherry",
+    "Plum",
+    "Raspberry",
+    "Blackberry",
+    "Cranberry",
+    "Lemon",
+    "Lime",
+    "Coconut",
+    "Pomegranate",
+    "Fig",
+    "Grapefruit",
+    "Avocado",
+    "Apricot",
+    "Papaya",
+    "Guava",
+    "Passion Fruit",
+    "Lychee",
+    "Cantaloupe",
+    "Honeydew",
+    "Dragon Fruit",
+    "Persimmon",
+    "Kumquat",
+    "Nectarine",
+    "Tangerine",
+    "Star Fruit",
+    "Jackfruit",
+    "Clementine",
+    "Mangosteen",
+    "Plantain",
+    "Quince",
+    "Rambutan",
+    "Soursop",
+    "Tamarillo",
+    "Ugli Fruit",
+    "Ackee",
+    "Acai Berry",
+    "Boysenberry",
+    "Breadfruit",
+    "Bilberry",
+    "Carambola",
+    "Cherimoya",
+    "Crabapple",
+    "Damson",
+    "Elderberry",
+    "Feijoa",
+    "Goji Berry",
+    "Jujube",
+    "Loquat",
+    "Mulberry",
+    "Pawpaw",
+    "Prickly Pear",
+    "Salmonberry",
+    "Sapodilla",
+    "Sapote",
+    "Soursop",
+    "Tayberry",
+    "Thimbleberry",
+    "Yuzu",
+    "Arctic Raspberry",
+    "Barberry",
+    "Bergamot Orange",
+    "Bilimbi",
+    "Blackcurrant",
+    "Blood Orange",
+    "Cloudberry",
+    "Crowberry",
+    "Currant",
+    "Damson Plum",
+    "Date",
+    "Durian",
+    "Feijoa",
+    "Gac",
+    "Gooseberry",
+    "Huckleberry",
+    "Kiwano",
+    "Lingonberry",
+    "Miracle Fruit",
+    "Muscadine",
+    "Nance",
+    "Olive",
+    "Pequi",
+    "Pitaya",
+    "Rowan Berry",
+    "Salak",
+    "Sea Buckthorn",
+    "Surinam Cherry",
+    "Wax Jambu",
+    "White Currant",
+    "Yangmei",
+    "Yamamomo",
+    "Zalzalak",
+    "Ziziphus",
+];
+
+// console.log(fruits.length);
+
+
+// console.log(fruits.includes("Coconut"));
+
+// console.log(fruits.indexOf("Coconut"));
+
+// const adddipika = fruits.splice(19,0,"Dipika");
+// console.table(fruits);
+
+// const deletedipika = fruits.splice(19,104,"Dipika");
+// console.table(fruits);
+
+
+
+
+
+// const newfruits = fruits.map((fruits)=>{
+//     return fruits.toUpperCase();
+// })
+
+// console.log (fruits.uppercse());
+
+
+
+// fruits.forEach(fruits => {
+//     console.log(fruits.toUpperCase())
+// });
+
+// 
+
+
+fruits.sort();
+fruits.reverse();
+
+fruits.forEach(fruit => {
+    console.log( fruit.toUpperCase())
+});
+
+// console.log(newfruits.sort().toString());
